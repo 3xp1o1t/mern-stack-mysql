45cd622ac6505ba279c81f8b2a6225250a41b9fe
3xp1o1t <jes.monti.503@gmail.com>
2022-10-20 07:35:51 -0500

    Se agregaron los metodos CRUD restantes
    Se agrego el metodo crear, actualizar y eliminar.
    Se agregaron ejemplos de su uso al index.http

--- server/controllers/tasks.controllers.js
+++ server/controllers/tasks.controllers.js
@@ -1,46 +1,91 @@
 import { pool } from "../db.js";
 
 export const getTasks = async (req, res) => {
-
   try {
-    const [result] = await pool.query('SELECT * FROM tasks ORDER BY createdAt ASC');
+    const [result] = await pool.query(
+      "SELECT * FROM tasks ORDER BY createdAt ASC"
+    );
     res.json(result);
   } catch (error) {
     return res.status(500).json({
-      message: error.message
+      message: error.message,
     });
   }
-
 };
 
 export const getTask = async (req, res) => {
   try {
-    const {id} = req.params;
-    const [result] = await pool.query('SELECT * FROM tasks WHERE id = ?', id);
+    const { id } = req.params;
+    const [result] = await pool.query("SELECT * FROM tasks WHERE id = ?", id);
 
-    if (result.length === 0){
+    if (result.length === 0) {
       return res.status(404).json({
-        message: "Task  not found."
-      })
+        message: "Task  not found.",
+      });
     }
 
     res.json(result[0]);
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+export const createTask = async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    const [result] = await pool.query(
+      "INSERT INTO tasks(title, description) VALUES (?, ?)",
+      [title, description]
+    );
+
+    res.json({
+      id: result.insertId,
+      title,
+      description,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+export const updateTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await pool.query("UPDATE tasks SET ? where id = ?", [
+      req.body,
+      id,
+    ]);
+
+    res.json(result);
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+export const deleteTask = async (req, res) => {
+
+  try {
+    const {id} = req.params;
+    const [result] = await pool.query('DELETE FROM tasks WHERE id = ?', id);
+
+    if (result.affectedRows === 0){
+      return res.status(400).json({
+        message: "Tasks not found."
+      });
+    }
+
+    return res.sendStatus(204);
 
   } catch (error) {
     return res.status(500).json({
       message: error.message
     });
   }
-};
 
-export const createTask = (req, res) => {
-  res.send("Creando tarea");
-};
-
-export const updateTask = (req, res) => {
-  res.send("Actualizando tarea");
-};
-
-export const deleteTask = (req, res) => {
-  res.send("Eliminando tarea");
 };
