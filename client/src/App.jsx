0695e4586dae6628a3fba119ed6c3c54cb705960
3xp1o1t <jes.monti.503@gmail.com>
2022-10-21 07:47:59 -0500

    Se agrego la navegacion
    Se creo un componente para controlar la navegacion

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,18 @@ import {Route, Routes} from 'react-router-dom';
 import TasksPage from './pages/TasksPage';
 import TasksForm from './pages/TasksForm';
 import NotFound from './pages/NotFound';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
+  <>
+    <Navbar />
     <Routes>
       <Route path='/' element={<TasksPage />}></Route>
       <Route path='/new' element={<TasksForm />}></Route>
       <Route path='*' element={<NotFound />}></Route>
     </Routes>
+  </>
   )
 }
 
